94b3a8095be85af959010c15941460b93722f040
fflepestok <david.komarovskiy@mail.ru>
2023-01-07 21:11:53 +0300

    typejs

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('Hellow world')
+// Базовые арифметические операторы
+
+/*const width = 10;
+const height = 5;
+const space = width * height;
+console.log(space);
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2**3 // 2 * 2 * 2
+console.log(volume);
+
+// Строки
+const city = 'Moscow';
+const country = 'Russia';
+console.log(city + ', ' + country + ' ' + 5);
+*/
+
+// Операторы присваивания 
+
+/*
+let age = 18;
+age += 2;
+// age += 2; // age = age +2
+// age -= 2; // age = age -2
+// age *= 2; // age = age *2
+// age /= 2; // age = age /2
+// age++;    // age = age + 1
+// age--;    // age = age -1
+console.log(age)
+
+// Операторы сравнения
+const vasya = 20;
+console.log(age > vasia);
+console.log(age < vasia);
+console.log(age >= vasia);
+console.log(age <= vasia);
+console.log(age == vasia);
+
+/*
