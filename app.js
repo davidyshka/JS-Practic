97c26f84b093bdeea90a5765a1c58135f4bc74aa
fflepestok <david.komarovskiy@mail.ru>
2023-01-12 19:54:12 +0300

    js+

--- app.js
+++ app.js
@@ -37,4 +37,44 @@ console.log(age >= vasia);
 console.log(age <= vasia);
 console.log(age == vasia);
 
+*/
+
+/*const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
+
+let b;
+let c;
+
+// c = b = 100 + 50;
+console.log(typeof(b));
+console.log(typeof(c));
+*/
 /*
+let a = 5;
+let b = 5.6;
+console.log(typeof a)
+a = 'string'
+console.log(typeof a)
+*/
+
+const payRaiteUSD = 80;
+const projectHours = 40;
+const availableHours = (11-2) * 5;
+console.log('Смогу ли я работать' + ' ' + (availableHours > projectHours))
+console.log('Я возьму' + ' ' + (payRaiteUSD * projectHours + '$'));
+
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'David Komarovskiy';
+
+const template = author + ' Заказал ' + projectName + ' По цене ' + price + '$';
+const template2= `${author} заказал ${projectName} по цене ${price}$`;
+console.log(template)
+console.log(template2)
+
+
+
+
+
+
+console.log(hoursWork)
